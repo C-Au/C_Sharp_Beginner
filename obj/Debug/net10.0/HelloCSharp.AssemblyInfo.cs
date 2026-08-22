5071d1371b9b50a9e7e41e692ef536ea408d8399
Colin <aucolin@hotmail.com>
2026-08-22 15:48:42 -0700

    Add launch and task configurations for HelloCSharp project

--- obj/Debug/net10.0/HelloCSharp.AssemblyInfo.cs
+++ obj/Debug/net10.0/HelloCSharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50875101dbc5f01bf24f79695ef9deb710795557")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38ebbfd6cb83bd60aa17f56e79c76d9c381c9f87")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloCSharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
